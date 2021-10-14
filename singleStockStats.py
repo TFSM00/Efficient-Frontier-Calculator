4fc49c178a90939694e786a076bf39028c0ee009
TFSM00 <tiagomoreira317@gmail.com>
2021-10-14 19:11:58 +0100

    Added function to only get the stats to only one ticker at a time

--- singleStockStats.py
+++ singleStockStats.py
@@ -1,28 +1,35 @@
 import pandas as pd
+from scipy.stats.stats import skew
 import yahoo_fin.stock_info as yf
 import numpy as np
 import statistics as stats
 import scipy.stats as st
 
-ticker = "GOOG"
-data = yf.get_data(ticker, start_date="08/01/2004",end_date="10/31/2019", interval="1mo") # DATE IS MM/DD/YYYY
+ticker = "SPY"
 
-prices = data["adjclose"].tolist()
+def getStockStats(ticker):
 
-returns = []
+    data = yf.get_data(ticker, start_date="08/31/2004",end_date="10/31/2019", interval="1mo") # DATE IS MM/DD/YYYY
 
-for i in range(0, len(prices)-1):
-    returns.append(float((prices[i+1]/prices[i])-1))
+    prices = data["adjclose"].tolist()
 
-average = np.mean(returns)
-standardDeviation = stats.pstdev(returns)
-kurtosis = st.kurtosis(returns)
-sample = len(returns)
-actualKurtosis = float((((kurtosis*(sample-2)*(sample-3)/(sample-1)-6)/(sample+1)+3)))
-skewness = st.skew(returns, bias=True)
-jarque_bera_test, jarque_bera_p_value = st.jarque_bera(returns)
+    returns = []
 
-if "__name__" == "__main__": #if script runs as program not import
+    for i in range(0, len(prices)-1):
+        returns.append(float((prices[i+1]/prices[i])-1))
+
+    average = np.mean(returns)
+    standardDeviation = stats.pstdev(returns)
+    kurtosis = st.kurtosis(returns)
+    sample = len(returns)
+    actualKurtosis = float((((kurtosis*(sample-2)*(sample-3)/(sample-1)-6)/(sample+1)+3)))
+    skewness = st.skew(returns, bias=True)
+    jarque_bera_test, jarque_bera_p_value = st.jarque_bera(returns)
+
+    return [average, standardDeviation, kurtosis, sample, actualKurtosis, skewness, jarque_bera_test, jarque_bera_p_value]
+
+
+if "__name__" == "__name__": #if script runs as program not import
     print("Average Returns: "+str(average))
     print("Standard Deviation: "+str(standardDeviation))
     print("Kurtosis: "+str(kurtosis))
