8c3f9e8a86e9262d3d12e42f15a54f67305e95f1
TFSM00 <tiagomoreira317@gmail.com>
2021-10-19 14:01:55 +0100

    Added program to get the price table for a single stock

---
+++ singleStockPrice.py
@@ -0,0 +1,11 @@
+import yahoo_fin.stock_info as yf
+
+tick = "AAPL"
+
+def getStockPrice(ticker):
+    data = yf.get_data(ticker, start_date="08/31/2004",end_date="10/31/2019", interval="1mo") # DATE IS MM/DD/YYYY
+
+    return data
+
+if __name__=="__main__":
+    print(getStockPrice(tick))
