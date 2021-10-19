076878d7b57e2243a2a5ebdb9149cfad749a70b6
TFSM00 <tiagomoreira317@gmail.com>
2021-10-19 23:15:34 +0100

    Added program with function to create a summary on a linear regression of one stock, function to retrive manipulatable data and function to display linear regression stats when script is run as main

---
+++ singleStockLinearRegression.py
@@ -0,0 +1,107 @@
+import pandas as pd
+from portfolioActualReturns import portfolioActualReturns
+from marketRiskPremium import marketActualReturns
+import statsmodels.api as sm
+
+tickers = ["AAPL","GOOG","AMZN","MSFT","INTC","IBM","ORCL","CSCO","NVDA"]
+
+def singleStockLinearRegression_Summary(tickerList, ind):
+    
+    actual_returns = portfolioActualReturns(tickerList) #the stock has to be in the list
+    market_actual_returns = marketActualReturns()
+
+    ticker = tickerList[ind]
+    stockReturns = actual_returns[ticker]
+    market = market_actual_returns["SPY"]
+
+    data={ticker: stockReturns, "SPY":market}
+    df = pd.DataFrame.from_dict(data)
+
+    Y = df[ticker]
+    X=df["SPY"]
+
+    X = sm.add_constant(X)
+
+    model = sm.OLS(Y,X).fit()
+
+    summary = model.summary()
+
+    return summary
+
+
+
+def singleStockLinearRegression_Stats(tickerList, ind):
+    actual_returns = portfolioActualReturns(tickerList)
+    market_actual_returns = marketActualReturns()
+    
+    ticker = tickerList[ind]
+    
+    stockReturns = actual_returns[ticker]
+    market = market_actual_returns["SPY"]
+
+    data={ticker: stockReturns, "SPY":market}
+    df = pd.DataFrame.from_dict(data)
+
+    Y = df[ticker]
+    X=df["SPY"]
+
+    X = sm.add_constant(X)
+
+    model = sm.OLS(Y,X).fit()
+
+    alpha_beta = dict(model.params)
+
+    alpha_beta["Alpha"] = alpha_beta.pop("const")
+    alpha_beta["Beta"] = alpha_beta.pop("SPY")
+
+    p_values = dict(model.pvalues)
+    p_values["Alpha p-value"] = p_values.pop("const")
+    p_values["Beta p-value"] = p_values.pop("SPY")
+
+    r_squared = model.rsquared
+
+    return [alpha_beta,p_values,r_squared]
+
+def singleStockLinearRegression_StatsOutput(tickerList, ind): #wont accept just the stock
+    actual_returns = portfolioActualReturns(tickerList)
+    market_actual_returns = marketActualReturns()
+
+    ticker = tickerList[ind]
+
+    stockReturns = actual_returns[f"{ticker}"]
+    market = market_actual_returns["SPY"]
+
+    data={f"{ticker}": stockReturns, "SPY":market}
+    df = pd.DataFrame.from_dict(data)
+
+    Y = df[f"{ticker}"]
+    X=df["SPY"]
+
+    X = sm.add_constant(X)
+
+    model = sm.OLS(Y,X).fit()
+
+    alpha_beta = dict(model.params)
+
+    alpha_beta["Alpha"] = alpha_beta.pop("const")
+    alpha_beta["Beta"] = alpha_beta.pop("SPY")
+
+    p_values = dict(model.pvalues)
+    p_values["Alpha p-value"] = p_values.pop("const")
+    p_values["Beta p-value"] = p_values.pop("SPY")
+
+    r_squared = model.rsquared
+
+    print(f"Linear Regression Stats for {ticker}:")
+    print("============================"+"="*len(ticker)+"=")
+    print(f"Alpha = {alpha_beta.get('Alpha')}")
+    print(f"Alpha p-value = {p_values.get('Alpha p-value')}")
+    print(f"Beta = {alpha_beta.get('Beta')}")
+    print(f"Beta p-value = {p_values.get('Beta p-value')}")
+    print(f"R-Squared = {r_squared}")
+    print("============================"+"="*len(ticker)+"=")
+
+    
+
+if __name__=="__main__":
+    singleStockLinearRegression_StatsOutput(tickers,0)
