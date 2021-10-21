9fece77fa5bd0003914a475dcc0888537053329c
TFSM00 <tiagomoreira317@gmail.com>
2021-10-21 10:51:32 +0100

    Fixed S&P500 bug

--- marketRiskPremium.py
+++ marketRiskPremium.py
@@ -1,29 +1,35 @@
-from stockStatistics import stockReturnsList
+from stockStatistics import stockReturnsforSingle
 from tb3ms import TB3MS_RiskFree
 import numpy as np
 
 def marketActualReturns():
-    returns = stockReturnsList(["^GSPC"])
+    returns = stockReturnsforSingle("^GSPC")
 
     riskFree = TB3MS_RiskFree()
     riskFree.pop(0)
 
 
-    returns["^GSPC"] = returns["^GSPC"] - riskFree
+    riskPremium = []
+    for item1,item2 in zip(returns,riskFree):
+        riskPremium.append(float(item1 - item2))
 
-    return returns
+    return riskPremium
 
 def marketAverageRiskPremium():
-    returns = stockReturnsList(["^GSPC"])
+    returns = stockReturnsforSingle("^GSPC")
 
     riskFree = TB3MS_RiskFree()
     riskFree.pop(0)
 
 
-    riskPremium = returns["^GSPC"] - riskFree
+    riskPremium = []
+    for item1,item2 in zip(returns,riskFree):
+        riskPremium.append(float(item1) - float(item2))
+    
 
     return np.mean(riskPremium)
 
 
 if __name__=="__main__":
     print(marketActualReturns())
+    #print(marketAverageRiskPremium())
