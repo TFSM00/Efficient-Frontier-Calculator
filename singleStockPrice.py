6f9e316bd360ba9fe63b4eff607e40a411399288
TFSM00 <tiagomoreira317@gmail.com>
2021-10-21 10:53:21 +0100

    Debugging

--- singleStockPrice.py
+++ singleStockPrice.py
@@ -1,6 +1,6 @@
 import yahoo_fin.stock_info as yf
 
-tick = "NVDA"
+tick = "^GSPC"
 
 def getStockPrice(ticker):
     data = yf.get_data(ticker, start_date="08/31/2004",end_date="10/31/2019", interval="1mo") # DATE IS MM/DD/YYYY
