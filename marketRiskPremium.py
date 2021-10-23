998b679b1b6fd7a781b687979f05d647a58b8e9c
TFSM00 <tiagomoreira317@gmail.com>
2021-10-23 11:04:17 +0100

    Fixed bug that outputted dataframe instead of list and fixed floating point mistake

--- marketRiskPremium.py
+++ marketRiskPremium.py
@@ -3,7 +3,9 @@ from tb3ms import TB3MS_RiskFree
 import numpy as np
 
 def marketActualReturns():
-    returns = stockReturnsforSingle("^GSPC")
+    returns_raw = stockReturnsforSingle("^GSPC")
+    returns = returns_raw["^GSPC"].to_list()
+
 
     riskFree = TB3MS_RiskFree()
     riskFree.pop(0)
@@ -11,12 +13,13 @@ def marketActualReturns():
 
     riskPremium = []
     for item1,item2 in zip(returns,riskFree):
-        riskPremium.append(float(item1 - item2))
+        riskPremium.append(float(item1) - float(item2))
 
     return riskPremium
 
 def marketAverageRiskPremium():
-    returns = stockReturnsforSingle("^GSPC")
+    returns_raw = stockReturnsforSingle("^GSPC")
+    returns = returns_raw["^GSPC"].to_list()
 
     riskFree = TB3MS_RiskFree()
     riskFree.pop(0)
@@ -31,5 +34,5 @@ def marketAverageRiskPremium():
 
 
 if __name__=="__main__":
-    print(marketActualReturns())
-    #print(marketAverageRiskPremium())
+    #print(marketActualReturns())
+    print(marketAverageRiskPremium())
