4b6212efc7aaab2b3b08a01201d3456456609c05
TFSM00 <tiagomoreira317@gmail.com>
2021-11-10 19:02:12 +0000

    Added minimum volatility portfolio, max sharpe ratio portfolio and capital market line to the efficient frontier plot

--- EfficientFrontier.py
+++ EfficientFrontier.py
@@ -13,6 +13,7 @@ p_weights = [] # Define an empty array for asset weights
 ind_er = average(tickers)
 cov_matrix = stockCovariance(tickers)
 df = Average_StdDev_Data(tickers)
+rf = TB3MS_Data().get("Last Effective Monthly Rate")
 
 num_assets = 9
 num_portfolios = 10000
@@ -32,11 +33,31 @@ for counter, symbol in enumerate(df.columns.tolist()):
     data[symbol+' weight'] = [w[counter] for w in p_weights]
 portfolios  = pd.DataFrame(data)
 
+lenght = len(portfolios["Returns"])
+sharpe_ratios = []
 
-plt.figure(figsize=(12.2,12.2))
-plt.scatter(portfolios["Volatility"], portfolios["Returns"])
+for i in range(lenght):
+    sharpe_ratios.append((portfolios["Returns"][i]-rf)/portfolios["Volatility"][i])
+
+
+portfolios["Sharpe Ratio"] = sharpe_ratios
+min_vol_port = portfolios.iloc[portfolios["Volatility"].idxmin()]
+optimal_risky_port = portfolios.iloc[((portfolios["Returns"]-rf)/portfolios["Volatility"]).idxmax()]
+
+plt.subplots(figsize=[30,20])
+main = plt.scatter(portfolios["Volatility"], portfolios["Returns"], c=sharpe_ratios)
+plt.scatter(min_vol_port[1],min_vol_port[0], color="r", marker="o", s=100, label="Minimum Volatility Portfolio")
+plt.scatter(optimal_risky_port[1], optimal_risky_port[0], color='g', marker='o', s=100, label="Optimal Portfolio")
 plt.title("Efficient Frontier", fontsize=18)
 plt.xlabel("Volatility", fontsize=14)
 plt.ylabel("Returns", fontsize=14)
+
+index = sharpe_ratios.index(max(sharpe_ratios))
+
+plt.plot([0, portfolios["Volatility"][index]],[rf, portfolios["Returns"][index]], "-", linewidth=3, color ="green", label = "Capital Market Line")
+
 plt.grid()
+plt.colorbar(main)
+plt.legend()
+
 plt.show()
